9460bfab832ba949a7217ce1013f1d100c4db624
srocha <shane.e.rocha@gmail.com>
2023-11-02 12:02:52 -0600

    cleaned intellij VCS

--- src/main/java/app/app/BingoController.java
+++ src/main/java/app/app/BingoController.java
@@ -3,7 +3,7 @@ package app.app;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class BingoController {
     @FXML
     private Label welcomeText;
 
