2a23bf71a6d6037ec70471958e330afded30a0f8
srocha <shane.e.rocha@gmail.com>
2023-11-09 18:48:26 -0700

    added multiple card functionality

--- src/main/java/app/BingoApp.java
+++ src/main/java/app/BingoApp.java
@@ -8,11 +8,12 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class BingoApp extends Application {  // the main application
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(BingoApp.class.getResource("bingo-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 700);
-        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+        Scene scene = new Scene(fxmlLoader.load(), 800, 700);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         scene.getStylesheets().add("https://fonts.googleapis.com/css2?family=Agbalumo&display=swap");
 
         stage.setTitle("Animal Bingo");
